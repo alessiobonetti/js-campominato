430d7b64666cf234153fbac7798382b65afbf4e1
Alessio Bonetti <alesine87@gmail.com>
2020-08-31 15:51:06 +0200

    CampoMinato

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 
-var bombe = 5;
-//var tentativi = 100;
-var arrayNumeri = [1,2,3,4,5];
+var bombe = 16;
+var tryNumber = 10;
+//creo variabile array che contiene i numeri utent>
+var arrayUserNumbers = [];
 //funzione che genera un numero casuale tra 1 e 100
 function numGenerator (min, max) {
   return Math.floor(Math.random() * (max - min) + min);
@@ -9,43 +10,58 @@ function numGenerator (min, max) {
 
 //funzione che dato un array e un numero mi controlla che il numero non sia già nell'array
 function checkNumber (array, number){
-  var numberFound = array.includes(number);
+  var i=0 ;
+  var numberFound= false;
+  while (i < array.length && numberFound == false){
+    if (array[i] == number){
+      numberFound = true;
+    }
+    i++ ;
+  }
   return numberFound;
 }
 //creo variabile array che contiene i numeri bomba
 var bombNumbers = [];
 //ciclo che chiama la mia funzione di generazione dei numeri 16 volte  while(i<bombe)
-var index = 0;
-while (index<bombe) {
-  var bombRandomNumber = numGenerator (1, 10);
+
+while (bombNumbers.length<bombe) {
+  var bombRandomNumber = numGenerator (1, 100);
   var bombCheckNumber = checkNumber (bombNumbers, bombRandomNumber);
-  if (bombCheckNumber == true){
-    bombRandomNumber = numGenerator (1, 10);
-  }
-  else{
+  if (bombCheckNumber == false){
     bombNumbers.push(bombRandomNumber);
   }
-  index++;
 }
 console.log(bombNumbers);
     //check che il numero non sia duplicato
     //se non è duplicato, lo salvo nell'array bombe
 
-//creo variabile array che contiene i numeri utente
 
-//variabili booleana haPerso = false
 
-//ciclo di 100-16 volte  //for(i=0; i<tentativi-bombe; i++)
-  //prompt in cui chiedo all'utente un numero
-  //check che il numero sia compreso tra 1 e 100
-  //check che il numero non sia duplicato nell'array utente
-  //se non è duplicato, salvo il numero nell'array utente
-  //check che il numero non sia nell'array bomba
-  //se il numero è nell'array bomba -> alert hai perso
-      //alert con array utente .length -> numero di volte che l'utente ha inserito un numero corretto
-      //haPerso = true;
-      //esco dal ciclo
+//variabili boolean
+var explodedBomb = false
 
-//controllo se haPerso è false
-  //alert hai vinto
-  //alert con array utente .length -> numero di volte che l'utente ha inserito un numero corretto
+//ciclo di 100-16 volte
+  while (arrayUserNumbers.length < tryNumber && explodedBomb == false){
+    var userNumber = parseInt(prompt("Inserisci un numero compreso tra 1 e 100"));
+    // controllo inserimento
+    if (isNaN(userNumber) || userNumber > 100 || userNumber < 1){
+      alert("Attento hai inserito un numero errato");
+    }
+    else if (checkNumber(arrayUserNumbers, userNumber) == false){
+      if (checkNumber(bombNumbers, userNumber) == true){
+        explodedBomb = true;
+      }
+      else {
+        arrayUserNumbers.push(userNumber);
+      }
+    }
+  }
+  console.log(arrayUserNumbers);
+
+//Risulktato
+if (explodedBomb==true){
+  alert("Hai perso, hai vinto" + arrayUserNumbers.length + "volte in fila." );
+}
+else {
+  alert("Congratulazioni hai finito il gioco");
+}
